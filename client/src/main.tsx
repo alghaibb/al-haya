4df020bfa54c97c42b552ca0b18214d2ea7b8b95
Moudi <mjaderi97@gmail.com>
2024-01-24 14:15:33 +1100

    Implemented user authentication and registration flow

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,28 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+// @ts-expect-error
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import App from "./App.tsx";
+import Signup from "./pages/signup/index.tsx";
+
 import "./styles/globals.css";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/signup",
+        element: <Signup />,
+      },
+    ],
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router={router} />
 );
---
+++ client/src/utils/mutations/userMutations.ts
@@ -0,0 +1,35 @@
+import { gql } from '@apollo/client';
+
+// Mutation for user registration
+export const REGISTER_USER = gql`
+  mutation register($fullName: String!, $email: String!, $password: String!, $confirmPassword: String!) {
+    register(registerInput: {
+      fullName: $fullName,
+      email: $email,
+      password: $password,
+      confirmPassword: $confirmPassword
+    }) {
+      token
+      user {
+        _id
+        fullName
+      }
+    }
+  }
+`;
+
+// Mutation for user login
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(loginInput: {
+      email: $email,
+      password: $password
+    }) {
+      token
+      user {
+        _id
+        fullName
+      }
+    }
+  }
+`;
