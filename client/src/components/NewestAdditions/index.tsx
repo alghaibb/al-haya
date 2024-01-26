25e9518b17ce53cb70df9b2be06417fb0512d8bd
Moudi <mjaderi97@gmail.com>
2024-01-27 03:20:10 +1100

    Added newest additions section

---
+++ client/src/components/NewestAdditions/index.tsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from "react";
+import { simpleProduct } from "@/interface";
+import { Link } from "react-router-dom";
+import client from "@/sanityClient";
+
+import { Button } from "../ui/button";
+
+import "./newest.styles.css";
+
+const NewestAdditions = () => {
+  const [products, setProducts] = useState<simpleProduct[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const query = `*[_type == 'product'][0...5] | order(_createdAt desc) {
+        _id,
+        price,
+        title,
+        "slug": slug.current,
+        "categoryName": category->title,
+        "imageUrl": images[0].asset->url
+      }`;
+      const data = await client.fetch(query);
+      setProducts(data);
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="newestAdditions-container">
+      <h2 className="newestAdditions-title">Our Newest Additions</h2>
+      <div className="newestAdditions-grid">
+        {products.map((product) => (
+          <div key={product._id} className="newestAdditions-card">
+            <Link to={`/product/${product.slug}`}>
+              <img
+                src={product.imageUrl}
+                alt={product.title}
+                className="newestAdditions-image"
+              />
+              <div className="newestAdditions-info">
+                <h3 className="newestAdditions-product-title">
+                  {product.title}
+                </h3>
+                <p className="newestAdditions-product-price">
+                  ${product.price}
+                </p>
+              </div>
+            </Link>
+          </div>
+        ))}
+        <Link to="/products">
+          <Button className="w-full">See All</Button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default NewestAdditions;
--- client/src/pages/home/home.tsx
+++ client/src/pages/home/home.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/Hero";
 
 import "./home.styles.css";
+import NewestAdditions from "@/components/NewestAdditions";
 
 const home = () => {
   return (
     <div className="homeContainer">
       <Hero />
+      <NewestAdditions />
     </div>
   );
 };
