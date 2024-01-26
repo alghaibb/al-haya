544d50414ef5a3e6b7f639bd2303dbfd1a90788c
Moudi <mjaderi97@gmail.com>
2024-01-26 23:19:30 +1100

    deleted category/home page styles and components for fresh start

--- client/src/pages/home/home.tsx
+++ client/src/pages/home/home.tsx
@@ -1,57 +1,7 @@
-import { useState, useEffect } from "react";
-import client from "@/sanityClient";
+import React from "react";
 
-import "./home.style.css";
-import { Button } from "@/components/ui/button";
-import { Link } from "react-router-dom";
-import Category from "@/components/Category";
-
-interface HeroBanner {
-  imageUrl: string;
-}
-
-const Home = () => {
-  const [heroBanner, setHeroBanner] = useState<HeroBanner | null>(null);
-
-  useEffect(() => {
-    const query = `*[_type == "heroBanner"][0]{
-      "imageUrl": image.asset->url
-    }`;
-    client.fetch(query).then((data) => {
-      setHeroBanner(data);
-    });
-  }, []);
-
-  if (!heroBanner) {
-    return <div>Loading...</div>;
-  }
-
-  return (
-    <div className="homeContainer">
-      {heroBanner && (
-        <div className="heroBannerContainer">
-          <div className="heroBannerSlogan">
-            <h1>Elegance in Modesty</h1>
-            <p>Explore the Beauty of Islamic Attire</p>
-          </div>
-          <img
-            src={heroBanner.imageUrl}
-            alt="Hero Banner"
-            className="heroBanner"
-          />
-        </div>
-      )}
-      <Link to="/new-arrivals">
-        <Button className="shopNowBtn" size="sm">
-          Shop Now
-        </Button>
-      </Link>
-      <div className="shopByCategoryTextWrapper">
-        <h1 className="shopByCategoryText">Shop By Category</h1>
-      </div>
-      <Category />
-    </div>
-  );
+const home = () => {
+  return <div>home</div>;
 };
 
-export default Home;
+export default home;
