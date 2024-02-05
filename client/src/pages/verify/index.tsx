85293e445747352b2e95a14271e66eea000463c6
Moudi <mjaderi97@gmail.com>
2024-02-05 11:20:08 +1100

    Added basic successful account verification page

---
+++ client/src/pages/verify/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./verify.styles.css";
+
+const Verify = () => {
+  return (
+    <div className="verifyContainer">
+      <p>Your account has been successfully verified</p>
+    </div>
+  );
+};
+
+export default Verify;
